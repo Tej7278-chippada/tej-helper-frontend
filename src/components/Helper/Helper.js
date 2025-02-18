47f7aab02fd1b8c4695603306e0cfea0a8737223
Tej7278-chippada <tejachippada45@gmail.com>
2025-02-18 13:16:22 +0530

    toolbar added on Helper.js component. Footer removed from the layout.

--- src/components/Helper/Helper.js
+++ src/components/Helper/Helper.js
@@ -1,80 +1,113 @@
 // components/Helper/Helper.js
 import React from 'react';
-import { Box, Typography, Card,useMediaQuery, IconButton } from '@mui/material';
-// import { useNavigate } from 'react-router-dom';
+import {Box, Button, Toolbar, Typography, useMediaQuery } from '@mui/material';
 import Layout from '../Layout';
-// import apiClient from '../../utils/axiosConfig'; // Use axiosConfig here
 import { useTheme } from '@emotion/react';
-// import CreateGroup from './CreateGroup';
-// import JoinGroup from './JoinGroup';
-// import SkeletonGroups from './SkeletonGroups';
-import PersonAddRoundedIcon from '@mui/icons-material/PersonAddRounded';
-import Diversity2RoundedIcon from '@mui/icons-material/Diversity2Rounded';
+import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
+import FilterListIcon from "@mui/icons-material/FilterList";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PostAddRoundedIcon from '@mui/icons-material/PostAddRounded';
 
 const Helper = ()=> {
-    const tokenUsername = localStorage.getItem('tokenUsername');
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const tokenUsername = localStorage.getItem('tokenUsername');
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-    return (
-        <Layout username={tokenUsername}>
-        <Card sx={{
-            flex: 1.5, margin:1,
-            height: '80vh', // Fixed height relative to viewport
-            overflowY: 'auto',
-            bgcolor: 'white', // Card background color (customizable)
-            borderRadius: 2, // Card border radius (customizable)
-            // boxShadow: 3, // Shadow for a modern look
-            scrollbarWidth: 'none'
-          }}>
+  return (
+    <Layout username={tokenUsername}>
+      <Box>
+      <Toolbar > 
+          <Typography variant="h6" style={{ flexGrow: 1, marginRight: '2rem' }}>
+            Posts
+          </Typography>
+          <Button
+            variant="contained"
+            // onClick={() => openUserProfile()}
+            sx={{
+              backgroundColor: '#1976d2', // Primary blue
+              color: '#fff',
+              padding: '8px 16px',
+              borderRadius: '24px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+              '&:hover': {
+                backgroundColor: '#1565c0', // Darker shade on hover
+              },
+              display: 'flex',
+              alignItems: 'center',
+              gap: '8px', marginRight: '10px'
+            }}
+          >
+            <PostAddRoundedIcon sx={{ fontSize: '20px' }} />
+            {/* <span style={{ fontSize: '14px', fontWeight: '500' }}>Add Product</span> */}
+          </Button>
+          <Button
+            variant="contained"
+            // onClick={handleFilterToggle}
+            sx={{
+              backgroundColor: '#1976d2', // Primary blue
+              color: '#fff',
+              padding: '8px 16px',
+              borderRadius: '24px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+              '&:hover': {
+                backgroundColor: '#1565c0', // Darker shade on hover
+              },
+              display: 'flex',
+              alignItems: 'center',
+              gap: '8px', marginRight: '10px'
+            }}
+          >
+            <FilterListIcon sx={{ fontSize: '20px' }} />
+            {/* <span style={{ fontSize: '14px', fontWeight: '500' }}>Filter</span> */}
+          </Button>
+          <Button
+            variant="contained"
+            // onClick={() => navigate('/wishlist')}
+            sx={{
+              backgroundColor: '#1976d2', // Primary blue
+              color: '#fff',
+              padding: '8px 16px',
+              borderRadius: '24px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+              '&:hover': {
+                backgroundColor: '#1565c0', // Darker shade on hover
+              },
+              display: 'flex',
+              alignItems: 'center',
+              gap: '8px', marginRight: '10px'
+            }}
+          >
+            <FavoriteIcon sx={{ fontSize: '20px' }} />
+            {/* <span style={{ fontSize: '14px', fontWeight: '500' }}>Filter</span> */}
+          </Button>
+          <Button
+            variant="contained"
+            // onClick={() => navigate("/my-orders")}
+            sx={{
+              backgroundColor: '#1976d2', // Primary blue
+              color: '#fff',
+              padding: '8px 16px',
+              borderRadius: '24px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+              '&:hover': {
+                backgroundColor: '#1565c0', // Darker shade on hover
+              },
+              display: 'flex',
+              alignItems: 'center',
+              gap: '8px', 
+            }}
+          >
+            <LocalMallRoundedIcon sx={{ fontSize: '20px' }} />
+            {/* <span style={{ fontSize: '14px', fontWeight: '500' }}>Filter</span> */}
+          </Button>
+          
+        </Toolbar>
 
-            <Box height={isMobile ? "77vh" : "auto"} sx={{ padding: '0px' }}>
-              <Box
-                position="sticky" //fixed
-                top={0}
-                left={0}
-                right={0}
-                zIndex={10}
-                sx={{
-                  bgcolor: 'white', // Background color to ensure visibility
-                  boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)', // Optional shadow for separation
-                  padding: '8px 16px', // Padding for a clean look
-                }}
-              >
-                <Box display="flex" justifyContent="space-between" alignItems="center">
-                  <Typography position="relative" variant="h5">Posts</Typography>
-                  <Box>
-                    <IconButton
-                      color="default"
-                    //   onClick={() => setOpenCreateGroup(true)}
-                      sx={{ mr: 1 }}
-                    >
-                      <Diversity2RoundedIcon />
-                    </IconButton>
-                    <IconButton
-                      color="default"
-                    //   onClick={() => setOpenJoinGroup(true)}
-                    >
-                      <PersonAddRoundedIcon />
-                    </IconButton>
-                  </Box>
-                </Box>
-              </Box>
-              <Box bgcolor="#f5f5f5"
-                // mt="64px" // Matches the approximate height of the fixed header
-                height="calc(80vh - 64px)" // Adjust the height of the scrollable area
-                sx={{
-                  overflowY: 'auto',
-                  paddingInline: isMobile ? '4px' : '6px', scrollbarWidth: 'none'
-                }}
-              >
-                <Box style={{ paddingTop: '8px', paddingBottom: '1rem' }}>
-                </Box>
-              </Box>
-            </Box>
-          </Card>
-          </Layout>
-    );
+
+      </Box>
+    
+    </Layout>
+  );
 
 
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,8 +10,48 @@ const Layout = ({ children, username }) => {
       <div style={{ flex: 0 }}>
         {children}
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
+    // <Box display="flex" flexDirection="column" height="100vh">
+    //   {/* Upper Toolbar */}
+    //   <Box
+    //     display="flex"
+    //     justifyContent="space-around"
+    //     alignItems="center"
+    //     p={1}
+    //     bgcolor={theme.palette.background.paper}
+    //     boxShadow={2}
+    //   >
+    //     <IconButton color="primary">
+    //       <HomeIcon />
+    //     </IconButton>
+    //     <IconButton color="primary">
+    //       <SettingsIcon />
+    //     </IconButton>
+    //   </Box>
+
+    //   {/* Content Area */}
+    //   <Box flexGrow={1} display="flex" justifyContent="center" alignItems="center">
+    //     {/* Main Content Goes Here */}
+    //   </Box>
+
+    //   {/* Bottom Toolbar */}
+    //   <Box
+    //     display="flex"
+    //     justifyContent="space-around"
+    //     alignItems="center"
+    //     p={1}
+    //     bgcolor={theme.palette.background.paper}
+    //     boxShadow={2}
+    //   >
+    //     <IconButton color="primary">
+    //       <InfoIcon />
+    //     </IconButton>
+    //     <IconButton color="primary">
+    //       <HelpIcon />
+    //     </IconButton>
+    //   </Box>
+    // </Box>
   );
 };
 
