050c1c66cb5b009c44656a7202b108aed64f1c36
Tej7278-chippada <tejachippada45@gmail.com>
2025-03-14 20:45:17 +0530

    SkeletonCards.js component ui updated and cards length updated for isMobile 👍

--- src/components/Helper/SkeletonCards.js
+++ src/components/Helper/SkeletonCards.js
@@ -1,14 +1,22 @@
-import { Card, CardContent, Grid, Skeleton } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { Card, CardContent, Grid, Skeleton, useMediaQuery } from "@mui/material";
 import React from "react";
 
-const SkeletonCards = () => (
+
+
+
+const SkeletonCards = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  
+  return (
     <Grid container spacing={2}>
-      {Array.from({ length: 12 }).map((_, index) => (
+      {Array.from({ length: isMobile ? 6 : 12 }).map((_, index) => (
         <Grid item xs={12} sm={6} md={4} key={index}>
           <Card style={{
                       borderRadius: '8px'
                     }}>
-            <Skeleton variant="rectangular" height={200} style={{ borderRadius: '8px' }} />
+            <Skeleton variant="rectangular" height={160} style={{ borderRadius: '8px' }} />
             <CardContent>
               <Skeleton variant="text" height={30} width="60%" />
               <Skeleton variant="text" height={20} width="80%" />
@@ -18,6 +26,6 @@ const SkeletonCards = () => (
         </Grid>
       ))}
     </Grid>
-  );
+  )};
 
 export default SkeletonCards;
