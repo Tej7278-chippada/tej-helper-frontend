3bbac91987df808a88a93339990f461c75ea238f
Tej7278-chippada <tejachippada45@gmail.com>
2025-06-16 23:28:29 +0530

    index.html manifest.json reverted 👍

--- src/components/Helper/LazyBackgroundImage.js
+++ src/components/Helper/LazyBackgroundImage.js
@@ -33,8 +33,7 @@ const LazyBackgroundImage = ({ base64Image, alt, children }) => {
 
   const imageSrc = base64Image 
     ? `data:image/jpeg;base64,${base64Image}` 
-    // : 'https://placehold.co/600x400?text=No+Image';
-    : 'null';
+    : 'https://placehold.co/600x400?text=No+Image';
 
     {!isLoaded && (
         <div style={{
